632f8674faeadd0043989d56af33158f3f887551
Exprezzzor <132411298+Exprezzzo@users.noreply.github.com>
2025-07-07 03:30:25 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+      '@/lib': path.resolve(__dirname, 'lib'),
+    };
+    return config;
+  }
+};
+
+module.exports = nextConfig;
